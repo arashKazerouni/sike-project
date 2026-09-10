24e2ac54f178495d99cb8ee2d923b882ff2fa75c
Arash Kazerouni <94610980+arashKazerouni@users.noreply.github.com>
2026-09-10 21:54:57 +0330

    fix: add login route

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { Navigation } from "@/components/navigation";
+import { Footer } from "@/components/footer";
+import { AuthPanel } from "@/components/auth-panel";
+
+export default function LoginPage() {
+  return (
+    <>
+      <Navigation />
+      <AuthPanel />
+      <Footer />
+    </>
+  );
+}
